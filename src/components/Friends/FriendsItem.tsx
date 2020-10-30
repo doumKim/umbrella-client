cb79fb1dbfa8b2e087e48d3c14c257cae71b93ab
billowycloud <bjkim0228@naver.com>
2020-10-30 11:06:27 +0900

    [Client] /#48/ feature-change-info
    ETC API 적용완료(아이디변경, 이미지변경, 닉네임변경)

--- src/components/Friends/FriendsItem.tsx
+++ src/components/Friends/FriendsItem.tsx
@@ -66,7 +66,7 @@ type NameProps = {
 const ImageWrapper = styled.View`
   border-radius: 20px;
   overflow: hidden;
-  border: 1px solid rgba(0, 0, 0, 0.3);
+  border: 1px solid rgba(0, 0, 0, 0.1);
 `;
 const Profile: React.FC<NameProps> = ({ name, avatar }: NameProps) => {
   return (
