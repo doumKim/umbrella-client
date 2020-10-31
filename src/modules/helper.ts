1dcb3950c1256f4f0fa581b6c6ab947b7178eb16
doumKim <eha0129@naver.com>
2020-11-01 01:22:15 +0900

    [Client] /#69/ Fix: handle empty schedule error
    친구에게 공유받은 일정이 없는 경우 발생하는 경고 예외 처리 완료
    
    Empty 컴포넌트 랜더링 처리

--- src/modules/helper.ts
+++ src/modules/helper.ts
@@ -1,24 +1,28 @@
 import { ScheduleType, TodoType } from '../api/schedule';
 
-export const sortSchedules = (schedules: ScheduleType[]): ScheduleType[] => {
-  const copiedSchedule = [...schedules];
-  copiedSchedule.sort(
-    (prevSchedule: ScheduleType, nextSchedule: ScheduleType): number => {
-      return prevSchedule.date > nextSchedule.date ? 1 : -1;
-    }
-  );
-  for (let i = 0; i < copiedSchedule.length; i++) {
-    copiedSchedule[i].todos.sort(
-      (prevTodo: TodoType, nextTodo: TodoType): number => {
-        if (prevTodo.hour > nextTodo.hour) {
-          return 1;
-        } else if (prevTodo.hour < nextTodo.hour) {
-          return -1;
-        } else {
-          return prevTodo.minutes > nextTodo.minutes ? 1 : -1;
-        }
+export const sortSchedules = (
+  schedules: ScheduleType[]
+): ScheduleType[] | undefined => {
+  if (schedules) {
+    const copiedSchedule = [...schedules];
+    copiedSchedule.sort(
+      (prevSchedule: ScheduleType, nextSchedule: ScheduleType): number => {
+        return prevSchedule.date > nextSchedule.date ? 1 : -1;
       }
     );
+    for (let i = 0; i < copiedSchedule.length; i++) {
+      copiedSchedule[i].todos.sort(
+        (prevTodo: TodoType, nextTodo: TodoType): number => {
+          if (prevTodo.hour > nextTodo.hour) {
+            return 1;
+          } else if (prevTodo.hour < nextTodo.hour) {
+            return -1;
+          } else {
+            return prevTodo.minutes > nextTodo.minutes ? 1 : -1;
+          }
+        }
+      );
+    }
+    return copiedSchedule;
   }
-  return copiedSchedule;
 };
